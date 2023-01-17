3f8e825d05882ce3de2ec78f1446e28a0f62b525
wogns4004 <wogns4004@naver.com>
2023-01-17 09:16:47 +0900

    cote

---
+++ src/inflearn/Lv5_4.java
@@ -0,0 +1,28 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.TreeSet;
+
+public class Lv5_4 {
+	public int solution(String str) {
+		int answer= 0;
+		Stack<Character> stack = new Stack<>();
+		
+		
+
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Lv5_4 T = new Lv5_4();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
+	}
+}
+
